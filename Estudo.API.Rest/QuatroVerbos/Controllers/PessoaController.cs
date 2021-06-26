7ddc64f3d2a3fddee423b55ddbdb6e873490f6fd
Vanderson Pereira <vanderson.aparecido@pro22.com.br>
2021-06-26 14:32:25 -0300

    Adiciona um projeto para tratar dos estudos dos 4 principais verbos HTTP.

---
+++ Estudo.API.Rest/QuatroVerbos/Controllers/PessoaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuatroVerbos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Estudo.API.Rest/QuatroVerbos/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuatroVerbos.Models
+{
+    public class Pessoa
+    {
+        public long Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public long Endereco { get; set; }
+        public long Genero { get; set; }
+    }
+}
